4c11c0f12710b5cd5eb259d229718c2e09054062
Zohvek <hatnice42@gmail.com>
2019-08-23 15:58:02 -0500

    adding post creation capabilitites

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -6,8 +6,10 @@ import { Component } from '@angular/core';
     templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent{
- newPost = 'No Content';
+    enteredValue = '';
+    newPost = 'No Content';
     onAddPost() {
-        this.newPost = 'The user\'s post';
+       
+        this.newPost = this.enteredValue;
     }
 }
