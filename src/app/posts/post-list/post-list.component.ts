a052a00446848f387c6f3047887e2182cd5c8368
Zohvek <hatnice42@gmail.com>
2019-08-26 13:24:01 -0500

    adding posts, adding more angular material styling

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+    posts = [
+        {title: 'first post', content: 'This is the first post'},
+        {title: 'second post', content: 'This is the second post'},
+        {title: 'third post', content: 'This is the third post'}
+    ];
+}
